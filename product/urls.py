37e2b2ceb895d090e60a5e5d018f1a7fa42c52ea
tailacristtina <tailacristina18866@gmail.com>
2025-11-28 19:55:26 -0300

    Delete product directory

--- product/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.listar_produtos, name='listar_produtos'),
-    path('carrinho/', views.ver_carrinho, name='ver_carrinho'),
-    path('carrinho/adicionar/<int:produto_id>/', views.adicionar_ao_carrinho, name='adicionar_ao_carrinho'),
-     path('finalizar/', views.finalizar_pedido, name='finalizar_pedido'),
-]
